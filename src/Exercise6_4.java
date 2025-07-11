55ece49625ca9af31d168d801a5dbdfe57127562
meaningGitt <swhj0824@swu.ac.kr>
2025-07-11 18:20:03 +0900

    first commit
    commit

---
+++ src/Exercise6_4.java
@@ -0,0 +1,7 @@
+public class Exercise6_4 {
+    public static void main(String[] args) {
+        Student s =new Student();
+        s.name = "홍길동";
+        s.ban = 1;
+    }
+}
---
+++ src/SutdaCard.java
@@ -0,0 +1,20 @@
+public class SutdaCard {
+    int num = (int) (Math.random() * 10) + 1;
+    boolean isKwang;
+
+    public SutdaCard(int num, boolean isKwang){
+        this.num = num;
+        this.isKwang = isKwang;
+        if (isKwang)
+            System.out.println("광");
+    }
+    public String toString(){
+        return num + (isKwang ? "K" : "");
+    }
+}
+class Exercise6_2{
+    public static void main(String[] args) {
+        SutdaCard card1 = new SutdaCard();
+        SutdaCard card2 = new SutdaCard();
+    }
+}
